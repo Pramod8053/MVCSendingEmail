7d8ed016c47128876f90351a0b9314bae58954f5
pramod <pbehera817@gmail.com>
2024-02-21 11:54:26 +0530

    Sending mail

--- MVCSendingEmail/MVCSendingEmail/Controllers/HomeController.cs
+++ MVCSendingEmail/MVCSendingEmail/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,7 +14,44 @@ namespace MVCSendingEmail.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("pbehera817@gmail.com", "Jamil");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "bdapb5182q";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
